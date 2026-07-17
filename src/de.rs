591d2cf1f95a0993b63c902a402cc17930e0fb27
Kimiblock Moe <kimiblock@icloud.com>
2026-07-17 19:40:08 +0800

    de: implement some types

--- src/de.rs
+++ src/de.rs
@@ -18,3 +18,37 @@ impl <'de> Deserializer <'de> {
 		Deserializer { input }
 	}
 }
+
+pub fn from_str<'a, T> (s: &'a str) -> Result<T, Error>
+	where T: Deserialize<'a>,
+{
+	let mut deserializer = Deserializer::from_str(s);
+	let t = T::deserialize(&mut deserializer)?;
+	if deserializer.input.is_empty() {
+		Ok(t)
+	} else {
+		Err(Error::TrailingCharacters)
+	}
+}
+
+impl <'de> Deserializer <'de> {
+	fn peek_char(&mut self) -> Result<char, Error> {
+		self.input.chars().next().ok_or(Error::EOF)
+	}
+	fn next_char(&mut self) -> Result<char, Error> {
+		let cha = self.peek_char()?;
+		self.input = &self.input[cha.len_utf8()..];
+		Ok(cha)
+	}
+	fn parse_bool(&mut self) -> Result<bool, Error> {
+		if self.input.starts_with("true") {
+			self.input = &self.input["true".len()..];
+			Ok(true)
+		} else if self.input.starts_with("false") {
+			self.input = &self.input["false".len()..];
+			Ok(false)
+		} else {
+			Err(Error::ExpectedBoolean)
+		}
+	}
+}
--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,16 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum Error {
 	#[error("failed reading configuration: {0:#?}")]
-	Message(String)
+	Message(String),
+
+	#[error("trailing characters")]
+	TrailingCharacters,
+
+	#[error("end of file")]
+	EOF,
+
+	#[error("expected boolean")]
+	ExpectedBoolean,
 }
 
 impl serde::de::Error for Error {
