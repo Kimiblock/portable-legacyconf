6fc7b5cd9db2ebe80de8e8fe97d68b2a1730bccb
Kimiblock Moe <kimiblock@icloud.com>
2026-07-18 14:21:33 +0800

    de: implement i8 deserialising

--- src/de.rs
+++ src/de.rs
@@ -174,4 +174,14 @@ impl <'de, 'a> de::Deserializer <'de> for &'a mut Deserializer <'de> {
 			self.parse_bool()?
 		)
 	}
+
+	fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+	where
+		V: Visitor<'de>
+	{
+		self.skip_whitespaces_and_comments()?;
+		visitor.visit_i8(
+			self.parse_signed()?
+		)
+	}
 }
