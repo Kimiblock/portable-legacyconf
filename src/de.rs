078cdacbe11138092187db130d1c0f2fea39051e
Kimiblock Moe <kimiblock@icloud.com>
2026-07-18 14:17:01 +0800

    de: stop skipping on real chars

--- src/de.rs
+++ src/de.rs
@@ -128,7 +128,9 @@ impl <'de> Deserializer <'de> {
 						}
 					}
 				}
-				_				=> {}
+				_				=> {
+					return Ok(());
+				}
 			}
 		}
 	}
@@ -141,6 +143,7 @@ impl <'de, 'a> de::Deserializer <'de> for &'a mut Deserializer <'de> {
 	fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
 		where V: Visitor<'de>
 	{
+		self.skip_whitespaces_and_comments();
 		match self.peek_char()? {
 			'"'	=> {
 				let s = self.parse_string()?;
