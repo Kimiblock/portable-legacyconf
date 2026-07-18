536fe6aa9057394dc03be8ee7b7027a29ece3ed9
Kimiblock Moe <kimiblock@icloud.com>
2026-07-18 14:33:56 +0800

    de: implement deserialisation of u64

--- src/de.rs
+++ src/de.rs
@@ -244,4 +244,14 @@ impl <'de, 'a> de::Deserializer <'de> for &'a mut Deserializer <'de> {
 			self.parse_unsigned()?
 		)
 	}
+
+	fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+	where
+		V: Visitor<'de>
+	{
+		self.skip_whitespaces_and_comments()?;
+		visitor.visit_u64(
+			self.parse_unsigned()?
+		)
+	}
 }
