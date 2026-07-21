91f9e5c590f20427d262af8094dbfc353b0cb451
Kimiblock Moe <kimiblock@icloud.com>
2026-07-21 16:34:22 +0800

    de: remove unused imports

--- src/de.rs
+++ src/de.rs
@@ -2,8 +2,7 @@ use std::ops::{AddAssign, MulAssign, Neg};
 
 use serde::Deserialize;
 use serde::de::{
-	self, DeserializeSeed, EnumAccess, IntoDeserializer, MapAccess, SeqAccess,
-	VariantAccess, Visitor,
+	self, DeserializeSeed, MapAccess, Visitor,
 };
 
 
